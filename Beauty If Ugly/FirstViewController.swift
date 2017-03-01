4d36fef29648109d121d9d512eb1940e97516a00
diplomaliformatci <diplomaliformatci@gmail.com>
2017-03-01 19:47:29 +0300

    Initial Commit

---
+++ Beauty If Ugly/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Beauty If Ugly
+//
+//  Created by Can KINCAL on 01/03/2017.
+//  Copyright © 2017 Can KINCAL. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
